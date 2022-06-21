a2271c090680722f81a3855e3a7f47660dbd7a92
vcaracol <vcaracol@SurfacePro8.localdomain>
2022-06-20 21:58:29 -0700

    bootcamp project done

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,65 @@
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new { |h, k| h[k] = [] }
+    end
+
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        @teachers << teacher
+    end
+
+    def enroll(student)
+        if students.length < @student_capacity
+            @students << student 
+            return true
+        end
+        false
+    end
+
+    def enrolled?(student)
+        @students.include?(student)
+    end
+
+    def student_to_teacher_ratio
+        @students.length / @teachers.length
+    end
+
+    def add_grade(student, grade)
+        if self.enrolled?(student)
+            @grades[student] << grade
+            return true
+        end
+        false
+    end
+
+    def num_grades(student)
+        @grades[student].length
+    end
+
+    def average_grade(student)
+        if self.enrolled?(student) && self.num_grades(student) > 0
+            return @grades[student].sum / @grades[student].length
+        end
+        nil
+    end
 end
--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,4 +1,6 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+    def span
+
+    end
 end
